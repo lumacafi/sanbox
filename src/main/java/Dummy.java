b32257b40483f881c9710a83073914e963157711
Luis Figueiredo <lumacafi@gmail.com>
2015-09-22 13:42:10 -0300

    Branch

--- src/main/java/Dummy.java
+++ src/main/java/Dummy.java
@@ -1,10 +1,10 @@
 /**
- * 
+ *
  * Created by luis on 9/22/15.
  */
 public class Dummy {
 
     public static void main(String[] args) {
-        System.out.println(12);
+        System.out.println(11);
     }
 }
