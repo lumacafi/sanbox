aea4eff2263f1f304252f5b89dad01d7c4d28c55
Luis Figueiredo <lumacafi@gmail.com>
2015-09-24 17:58:39 -0300

    Added gradle

--- src/main/java/Dummy.java
+++ src/main/java/Dummy.java
@@ -1,10 +1,11 @@
+import java.util.Date;
+
 /**
- *
  * Created by luis on 9/22/15.
  */
 public class Dummy {
 
     public static void main(String[] args) {
-        System.out.println(11);
+        System.out.println(new Date());
     }
 }
